0677d836f4c5eb2add7d1331a09ec94bd578b68f
Ruslan Deyarov <r.deyarov@applicaster.com>
2025-09-17 15:26:29 +0400

    added patch message queue

--- tvos/setup.sh
+++ tvos/setup.sh
@@ -57,3 +57,39 @@ sed -i '' 's/const IS_DEBUGGER_ENABLED = false;/const IS_DEBUGGER_ENABLED = true
 echo "✅ Activated debugger flag in $FILE_FOR_ACTIVATE_DEBUGGER"
 
 # Activate debugger - END
+
+# Patch MessageQueue - START
+
+FILE_FOR_PATCH_MESSAGE_QUEUE="./node_modules/react-native/Libraries/BatchedBridge/MessageQueue.js"
+
+if [ -z "$FILE_FOR_PATCH_MESSAGE_QUEUE" ]; then
+  echo "❌ Please provide a file path."
+  echo "Usage: $0 path/to/file.js"
+  exit 1
+fi
+
+if [ ! -f "$FILE_FOR_PATCH_MESSAGE_QUEUE" ]; then
+  echo "❌ File not found: $FILE_FOR_PATCH_MESSAGE_QUEUE"
+  exit 1
+fi
+
+# Use sed to replace the entire method (single-line form)
+# Replace the whole callNativeSyncHook method
+sed -i '' '/callNativeSyncHook(/,/^  }/c\
+  callNativeSyncHook(\
+    moduleID: number,\
+    methodID: number,\
+    params: mixed[],\
+    onFail: ?(...mixed[]) => void,\
+    onSucc: ?(...mixed[]) => void,\
+  ): mixed {\
+    this.processCallbacks(moduleID, methodID, params, onFail, onSucc);\
+    if(global.nativeCallSyncHook)\
+      {\
+        return global.nativeCallSyncHook(moduleID, methodID, params);\
+      }\
+  }' "$FILE_FOR_PATCH_MESSAGE_QUEUE"
+
+echo "✅ Replaced callNativeSyncHook method in $FILE_FOR_PATCH_MESSAGE_QUEUE"
+
+# Patch MessageQueue - END
